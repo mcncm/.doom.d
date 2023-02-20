abbf69127f9eecf7b6366ad1e38ff33f06a0edf1
mcncm <33966432+mcncm@users.noreply.github.com>
2023-02-20 15:04:41 -0500

    Add org-jupyter

--- init.el
+++ init.el
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        (ocaml +lsp)             ; an objective camel
-       (org +roam2)               ; organize your plain life in plain text
+       (org +roam2 +jupyter)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
